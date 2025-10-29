7212ea26800f1c0335143a7722ec2cfbaadd65b2
Paul J Rogers <pauljrogers@gmail.com>
2025-10-29 14:03:46 -0600

    Initial commit of Trip Weather Spring Boot application

---
+++ src/main/java/com/pjr22/tripweather/TripweatherApplication.java
@@ -0,0 +1,13 @@
+package com.pjr22.tripweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripweatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjr22/tripweather/TripweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjr22.tripweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
